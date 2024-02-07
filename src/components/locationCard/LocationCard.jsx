c238ea6e257f813526fe3e400d018fb9a456ddc2
kiana esmaili <kianaesmaili18@gmail.com>
2024-02-07 17:14:08 +0330

    feat : add lg responsive

--- src/components/locationCard/LocationCard.jsx
+++ src/components/locationCard/LocationCard.jsx
@@ -2,16 +2,16 @@
 function LocationCard(props) {
   console.log(props);
   return (
-    <div className="max-w-[20rem] min-w-[18.25rem] w-full h-[19rem] rounded-xl overflow-hidden m-1 hover:scale-105">
-      <span className="">
+    <div className="max-w-[20rem] min-w-[18.25rem] w-full h-[19rem] rounded-xl overflow-hidden m-1">
+      <span className="w-full h-[14rem] overflow-hidden block bg-red-600">
         <img
           src={props.picture_url.url}
           alt={location.name}
-          className="w-full max-h-[14rem] transition-all"
+          className="w-full h-[14rem] transition-all hover:scale-110 duration-300"
         />
       </span>
-      <h3 className="font-bold">{props.smart_location}</h3>
-      <span className="text-slate-500">{props.name}</span> <br />
+      <h3 className="font-bold mt-2 ">{props.smart_location}</h3>
+      <span className="text-slate-500 text-xs">{props.name}</span> <br />
       <span className="text-slate-500">
         <strong className="text-black">€ {props.price} </strong>
         night
--- src/components/locationList/LocationList.jsx
+++ src/components/locationList/LocationList.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import LocationCard from "../locationCard/LocationCard";
 
 import useFetch from "../../../utilities/useFetch/useFetch";
+import Loader from "../loader/Loader";
 
 function LocationList() {
   const { data, isLoading } = useFetch("");
   console.log(data, isLoading);
   // console.log(fff);
 
-  if (isLoading) return <div>is loading...</div>;
+  if (isLoading) return <Loader />;
   return (
-    <div className="flex items-start justify-center flex-wrap mt-8">
-      <span className="w-full text-left">Nearby Location</span>
-      {data?.map((location) => (
-        <LocationCard key={location.id} {...location} />
-      ))}
+    <div className="max-w-[120rem] mx-auto">
+      <span className="w-[80%] max-w-[84rem] mx-auto text-left mt-4 mb-2 block font-bold sm:text-xl lg:mt-6">
+        Nearby Location
+      </span>
+      <div className="flex items-start justify-center flex-wrap gap-2 lg:gap-4 lg:w-[80%] mx-auto">
+        {data?.map((location) => (
+          <LocationCard key={location.id} {...location} />
+        ))}
+      </div>
     </div>
   );
 }
