0d6dc0a86981df08880940f181ab50e5272bdc32
kiana esmaili <kianaesmaili18@gmail.com>
2024-02-10 18:20:00 +0330

    feat : add delete bookmark

--- src/components/context/BookmarkContext.jsx
+++ src/components/context/BookmarkContext.jsx
@@ -1,39 +1,73 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
-const BASE_URL = "http://localhost:3000/bookmarks/";
+const BASE_URL = "http://localhost:3000/bookmarks";
 const bookmarkContext = createContext();
 
 export default function BookmarksProvider({ children }) {
   const [bookmarksData, setBookmarksData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  function getAllBookmarks() {
-    setIsLoading(true);
-    try {
-      const { data } = axios.get(BASE_URL);
-      setBookmarksData(data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      alert(error.message);
+  useEffect(() => {
+    async function getAllBookmarks() {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get(BASE_URL);
+        console.log(data);
+        setBookmarksData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        alert(error.message);
+      }
     }
-  }
+    getAllBookmarks();
+  }, []);
 
-  function addNewBookmark(newBookmark) {
+  async function addNewBookmark(newBookmark) {
     try {
-      const { data } = axios.post(BASE_URL, newBookmark);
+      const { data } = await axios.post(BASE_URL, newBookmark);
       console.log(data);
     } catch (error) {
       alert(error.message);
     }
   }
 
+  async function deleteBookmark(id) {
+    try {
+      await axios.delete(`${BASE_URL}/${id}`);
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  const [currBookmark, setCurrBookmark] = useState({});
+
+  async function getCurrBookmark(id) {
+    setIsLoading(true);
+    try {
+      const { data } = await axios(`${BASE_URL}/${id}`);
+      setCurrBookmark(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      alert(error.message);
+    }
+  }
+
   return (
     <bookmarkContext.Provider
-      value={{ addNewBookmark, getAllBookmarks, isLoading, bookmarksData }}
+      value={{
+        addNewBookmark,
+        isLoading,
+        bookmarksData,
+        setBookmarksData,
+        deleteBookmark,
+        getCurrBookmark,
+        currBookmark,
+      }}
     >
       {children}
     </bookmarkContext.Provider>
